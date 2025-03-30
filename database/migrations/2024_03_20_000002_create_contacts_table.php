93a3a57d735444d584886493a31f376375833d93
Azzedine <azzedinerih2001@gmail.com>
2025-03-30 22:41:30 +0000

    message

--- database/migrations/2024_03_20_000002_create_contacts_table.php
+++ database/migrations/2024_03_20_000002_create_contacts_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
-            $table->string('prenom');
-            $table->string('phone');
-            $table->text('email')->unique();
+            // $table->string('prenom');
+            // $table->string('phone');
+            // $table->text('email')->unique();
             $table->string('entreprise');
             $table->integer('note_personnelle');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
